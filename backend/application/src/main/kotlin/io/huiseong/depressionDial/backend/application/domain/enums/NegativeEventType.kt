a59eb9b3a96b54b104d31801f93433a02ef138a5
shiniseong <hss275989@gmail.com>
2024-04-25 23:14:07 +0900

    feat: NegativeEvent Entity 정의

---
+++ backend/application/src/main/kotlin/io/huiseong/depressionDial/backend/application/domain/enums/NegativeEventType.kt
@@ -0,0 +1,6 @@
+package io.huiseong.depressionDial.backend.application.domain.enums
+
+enum class NegativeEventType(val value: Int) {
+    NORMAL(0),
+    RELAPSE_PREVENTION(1)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,14 +8,7 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
-    plugins {
-        kotlin("jvm") version "1.9.23"
-    }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
-
 
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
@@ -31,20 +24,9 @@ include(":backend:adapter:out:persistence")
 
 include(":backend:application")
 
-include(":backend:bootstrap")
 include(":backend:bootstrap:di")
 include(":backend:bootstrap:transaction")
-
 include(":backend:bootstrap:server")
 
-include(":backend:bootstrap:android")
-include(":backend:bootstrap:common")
-include(":backend:bootstrap:desktop")
-include(":backend:bootstrap:ios")
-include(":backend:bootstrap:web")
-
-include(":frontend:android")
-include(":frontend:common")
-include(":frontend:desktop")
-include(":frontend:ios")
+include(":frontend:composeApp")
 include(":frontend:web")
